9f84aeca57837380d830d4b1203821a5bb3db415
Aitor <aitorkohan@gmail.com>
2025-11-20 00:02:03 +0300

    Added CRUD endpoints
    Signed-off-by: Aitor <aitorkohan@gmail.com>

--- src/main/java/com/aitor/springwebapplication/controller/UserController.java
+++ src/main/java/com/aitor/springwebapplication/controller/UserController.java
@@ -1,25 +1,41 @@
 package com.aitor.springwebapplication.controller;
 
+import com.aitor.springwebapplication.dto.UserRequestTo;
+import com.aitor.springwebapplication.dto.UserResponseTo;
 import com.aitor.springwebapplication.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/")
-    public List<User> getAllUsers(){
+    public List<UserResponseTo> getAllUsers(){
         return new LinkedList<>();
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable String id){
-        return new User(id);
+    public UserResponseTo getById(@PathVariable String id){
+        return new UserResponseTo("asd", "dfs", "dfsd");
+    }
+
+    @PostMapping("/")
+    public UserResponseTo getById(@RequestBody UserRequestTo user){
+        return new UserResponseTo("asd", "dfs", "dfsd");
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseTo put(@PathVariable String id, @RequestBody User user){
+        return new UserResponseTo("asd", "dfs", "dfsd");
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+
     }
 }
