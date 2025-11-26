1f56e4a8e5dea8e9d6114af3e7fbea8740707d22
Aitor <aitorkohan@gmail.com>
2025-11-26 23:22:38 +0300

    jakarta.validation.constraints imports replaced with wildcard. Removed useless @Builder annotation
    Signed-off-by: Aitor <aitorkohan@gmail.com>

--- src/main/java/com/aitor/springwebapplication/dto/MessageRequestTo.java
+++ src/main/java/com/aitor/springwebapplication/dto/MessageRequestTo.java
@@ -1,13 +1,10 @@
 package com.aitor.springwebapplication.dto;
 
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MessageRequestTo {
